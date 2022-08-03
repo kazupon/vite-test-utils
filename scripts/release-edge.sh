a0eb3a5348c3beaaaf45e0d55fa25766e7a432c6
kazuya kawaguchi <kawakazu80@gmail.com>
2022-08-03 14:56:07 +0900

    add ci workflow with github action

---
+++ scripts/release-edge.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -xe
+
+# Restore all git changes
+git restore --source=HEAD --staged --worktree -- package.json pnpm-lock.yaml
+
+# Install pnpm
+pnpm install
+
+# Build packages
+pnpm build
+
+# Bump versions to edge
+npx jiti ./scripts/bump-edge
+
+# Update token
+if [[ ! -z ${NPM_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
+  npm whoami
+fi
+
+# Release packages
+echo "Publishing"
