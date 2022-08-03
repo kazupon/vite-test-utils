380f5e0ce069636c147d0665871b2c651ffb318c
kazuya kawaguchi <kawakazu80@gmail.com>
2022-08-03 23:29:14 +0900

    remove dummy

--- src/index.test.ts
+++
@@ -1,5 +0,0 @@
-import { say } from './index'
-
-test('say', () => {
-  expect(say('Hello World')).toBe('Hello World')
-})
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,3 @@ export { createPage } from './browser'
 export { url, fetch, $fetch } from './server'
 export { setup } from './setup'
 export * from './types'
-
-export function say(msg: string) {
-  console.log(msg)
-  return msg
-}
