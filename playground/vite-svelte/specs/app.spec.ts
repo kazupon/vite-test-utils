3a388f684069b39a031a9d3b352f459f3a6c2b90
kazuya kawaguchi <kawakazu80@gmail.com>
2022-08-09 20:23:19 +0900

    add svelte (svelte-kit) example

---
+++ playground/vite-svelte/specs/app.spec.ts
@@ -0,0 +1,20 @@
+import { setup, createPage, fetch } from 'vite-test-utils'
+
+await setup()
+
+test('count', async () => {
+  const page = await createPage('/')
+
+  const button = await page.locator('button[aria-label="Increase the counter by one"]')
+  await button.focus()
+  await page.waitForTimeout(1000)
+  await button.click()
+  await page.waitForTimeout(500)
+  await button.click()
+  await page.waitForTimeout(500)
+  await button.click()
+  await page.waitForTimeout(500)
+
+  const count = await page.locator('.counter strong >> nth=-1')
+  expect(await count.textContent()).toBe('3')
+})
