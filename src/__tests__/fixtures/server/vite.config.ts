870307a6c67308d565b7dbb2128e507cfeb23cf8
kazupon <kazupon@users.noreply.github.com>
2022-09-04 17:15:23 +0000

    deploy: f6891bc68b26b8e0eef205ece19e67c385527b9b

--- src/__tests__/fixtures/server/vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite'
-import MyPlugin from './plugin'
-
-export default defineConfig({
-  plugins: [MyPlugin()]
-})
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { createPage } from './browser'
-export { url, fetch, $fetch, startServer, stopServer } from './server'
-export { setup } from './setup'
-export * from './types'
