602de55af5314af6f8bc1f6a86411eea00d10fde
kazuya kawaguchi <kawakazu80@gmail.com>
2022-09-05 03:58:54 +0900

    fix: relase workflow (#43)

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,11 +2,12 @@
 
 set -xe
 
-# Restore all git changes
-git restore --source=HEAD --staged --worktree -- package.json pnpm-lock.yaml
-
-# Install pnpm
-pnpm install --no-frozen-lockfile
+if [[ ! -z ${PNPM_INSTALL} ]] ; then
+  # Restore all git changes
+  git restore --source=HEAD --staged --worktree -- package.json pnpm-lock.yaml
+  # Install pnpm
+  pnpm install --no-frozen-lockfile
+fi
 
 # Check edge release
 if [[ ! -z ${EDGE_RELEASE} ]] ; then
